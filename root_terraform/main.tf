f5febb77465ca023dbdfe310a5415c1df2ddb3cf
lewelyn7 <karol.hamielec@gmail.com>
2021-07-09 10:37:06 +0200

    init

---
+++ root_terraform/main.tf
@@ -0,0 +1,63 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  access_key                  = "mock_access_key"
+  region                      = "us-east-1"
+  s3_force_path_style         = true
+  secret_key                  = "mock_secret_key"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+  alias = "dev"
+  endpoints {
+    apigateway     = "http://localhost:4567"
+    cloudformation = "http://localhost:4581"
+    cloudwatch     = "http://localhost:4582"
+    dynamodb       = "http://localhost:4569"
+    ec2            = "http://localhost:4597"
+    es             = "http://localhost:4578"
+    firehose       = "http://localhost:4573"
+    iam            = "http://localhost:4593"
+    kinesis        = "http://localhost:4566"
+    lambda         = "http://localhost:4574"
+    route53        = "http://localhost:4580"
+    redshift       = "http://localhost:4577"
+    s3             = "http://localhost:4572"
+    secretsmanager = "http://localhost:4584"
+    ses            = "http://localhost:4579"
+    sns            = "http://localhost:4575"
+    sqs            = "http://localhost:4576"
+    ssm            = "http://localhost:4583"
+    stepfunctions  = "http://localhost:4585"
+    sts            = "http://localhost:4592"
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+#   alias = "prod"
+}
+
+
+module "candle_getter" {
+    source = "../candle_getter/tf"
+}
+
+# resource "aws_kinesis_stream" "candle_data_stream" {
+#   name             = "candle_data"
+#   shard_count      = 1
+#   retention_period = 24
+
+
+#   tags = {
+#     Environment = "test"
+#   }
+# }
