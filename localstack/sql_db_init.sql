7859f49204ec1e740ca37b326044521aa55774ba
Natalia <natib13@gmail.com>
2021-12-11 20:57:56 +0100

    stock db

--- localstack/sql_db_init.sql
+++ localstack/sql_db_init.sql
@@ -35,3 +35,16 @@ then
 end if;
 END $
 DELIMITER ;
+
+DELIMITER $
+BEGIN NOT ATOMIC
+
+If not exists (SELECT 1 
+               FROM   INFORMATION_SCHEMA.SCHEMATA
+               WHERE  SCHEMA_NAME = 'stock_mock')
+then
+  CREATE DATABASE stock_mock;
+  grant all privileges on stock_mock.* to 'test'@'%';
+end if;
+END $
+DELIMITER ;
